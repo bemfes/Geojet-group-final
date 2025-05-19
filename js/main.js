12239ef16cc65858640ec53764790160a78fcec4
Anastasiia Ustin <2anastasiia@mail.ru>
2025-05-19 21:12:07 +0200

    added close-open mechanism for service items

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const formBox = document.querySelector('.form-service__box');
 const inputs = document.querySelectorAll('.form-service__input');
 const btnFormSend = document.querySelector('.btn_form-service');
 const btnCloseForm = document.querySelector('.form-service__close-btn');
+
 if (btnOpenForm) {
     btnOpenForm.addEventListener('click', () => {
         formBox.classList.add('form-service__box_open')
@@ -28,19 +29,30 @@ if (btnCloseForm) {
 }
 
 const sidebarItem = document.querySelectorAll('.sidebar__item');
+const sidebarItemDesc = document.querySelectorAll('.sidebar__item-desc');
+const sidebarItemName = document.querySelectorAll('.sidebar__item-name');
 if (sidebarItem) {
-    sidebarItem.forEach(item => {  
+    sidebarItem.forEach(item => { 
+        
         item.addEventListener('click', () => {
+            sidebarItemDesc.forEach(desc => {
+                desc.classList.add('sidebar__item-desc_hidden')
+                desc.style.maxHeight = 0
+            })
+            sidebarItemName.forEach(name => {
+                name.classList.remove('sidebar__item-name_open')    
+            })
             const desc = item.querySelector('.sidebar__item-desc')
-            const btn = item.querySelector('.sidebar__item-name')
-            if (desc.style.maxHeight) {
-                    desc.style.maxHeight = null
-                    btn.classList.remove('sidebar__item-name_open')
-                    desc.classList.add('sidebar__item-desc_hidden')
-                } else {
-                    desc.style.maxHeight = desc.scrollHeight + "px"
-                    btn.classList.add('sidebar__item-name_open')            
-        }
+            const name = item.querySelector('.sidebar__item-name')
+            const computed = getComputedStyle(desc).maxHeight[0]
+            if(computed >= 1) {
+                desc.style.maxHeight = 0
+                name.classList.remove('sidebar__item-name_open')    
+            } else {
+                desc.style.maxHeight = desc.scrollHeight + "px"
+                name.classList.add('sidebar__item-name_open')   
+            }
+            
         })
     })
 
